7e7fb7fbbd95aa98ed0dc4418b59196656fc4959
Chivier Humber <chivier.humber@gmail.com>
2021-12-19 17:26:10 +0800

    Ver 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # WWUtools
+
 WantWords Utools 插件， https://wantwords.thunlp.org/
+
+## 使用方法
+
+0. 了解[万词词典](https://wantwords.thunlp.org/)
+1. 复制你需要进行查询的词语
+2. 打开Utools，选择你需要进行的查询类型。
+   查询类型有：中中(wczz)，中英(wczy)，英中(wcyz)，英英(wcyy)
+3. （可选） 对你需要查询的词进行微调
+4. 按下回车
+
+## 效果截图
+![image1](images/image1.png)
--- preload.js
+++ preload.js
@@ -1,9 +1,63 @@
+const path = require('path')
+const fs = require('fs')
+const cp = require('child_process')
+let bookmarksDataCache = null
+let tabListCache = []
+
 window.exports = {
-    'python': {
-      mode: 'doc',
-      args: {
-        indexes: require('./indexes.json')
+  'wczz': {
+    mode: 'list',
+    args: {
+      enter: (action, callbackSetList) => {
+         window.utools.hideMainWindow()
+         utools.ubrowser.goto('https://wantwords.thunlp.org/')
+         .focus('#description')
+         .paste()
+         .run({ width: 1000, height: 1000 })
+         utools.outPlugin()
       }
     }
-  }
-  
+  },
+  'wczy': {
+   mode: 'list',
+   args: {
+     enter: (action, callbackSetList) => {
+        window.utools.hideMainWindow()
+        utools.ubrowser.goto('https://wantwords.thunlp.org/')
+        .click("#ui-id-2")
+        .focus('#description_CE')
+        .paste()
+        .run({ width: 1000, height: 1000 })
+        utools.outPlugin()
+     }
+   }
+ },
+ 'wcyz': {
+   mode: 'list',
+   args: {
+     enter: (action, callbackSetList) => {
+        window.utools.hideMainWindow()
+        utools.ubrowser.goto('https://wantwords.thunlp.org/')
+        .click("#ui-id-4")
+        .focus('#description_EC')
+        .paste()
+        .run({ width: 1000, height: 1000 })
+        utools.outPlugin()
+     }
+   }
+ },
+ 'wcyy': {
+   mode: 'list',
+   args: {
+     enter: (action, callbackSetList) => {
+        window.utools.hideMainWindow()
+        utools.ubrowser.goto('https://wantwords.thunlp.org/')
+        .click("#ui-id-3")
+        .focus('#description_EE')
+        .paste()
+        .run({ width: 1000, height: 1000 })
+        utools.outPlugin()
+     }
+   }
+ }
+}
